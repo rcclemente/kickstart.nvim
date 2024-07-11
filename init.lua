22d18dac428cb11a4cba3d600108003fdcb831c9
Ryan Clemente <kojiee@gmail.com>
2024-07-11 17:01:25 +0800

    add buffer to lsp

--- init.lua
+++ init.lua
@@ -796,6 +796,7 @@ require('lazy').setup({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'buffer' },
         },
       }
     end,
